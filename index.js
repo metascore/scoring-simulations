276064ec473debb4c9dac7592512a9631814fa4c
Jorgen <jorgen@buildnode.io>
2021-09-18 11:44:30 -0700

    % of max instead of percentile for normalization and overall ranking

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const defaultScoringConfig = {
     max: 1_000_000_000_000,
 
-    participationPortion: .2,
-    performancePortion: .3,
+    performancePortion: .5,
     podiumPortion: .5,
 
     secondPlaceDenominator: 2,
@@ -12,28 +11,38 @@ const defaultScoringConfig = {
 const defaultRankingConfig = {
     tier1ScoreThreshold: 1_000_000_000_000,
     tier2Percentile: .5,
-    tier3Percentile: .85,
+    tier3Percentile: .75,
 };
 
-simulate(5, 10000, {min: .25, max: 1, resolution: .1}, {min: .1, max: 1, resolution: .1});
+// simulate(10, 1000, {min: .25, max: 1, resolution: .1}, {min: .1, max: 1, resolution: .1});
+monteCarlo();
 
-// function monteCarlo (
-//     simsPerParameterSet     = 100,
-//     stepsPerParameterRange  = 10,
-//     numberOfGamesRange      = { min: 3,     max: 50,        resolution: 1, },
-//     numberOfPlayersRange    = { min: 100,   max: 100_000,   resolution: 1000, },
-//     playerEngagementRange   = { min: .1,    max: 1,         resolution: .1, },
-//     scoreRandomnessRange    = { min: 0,     max: 1,         resolution: .1, },
-// ) {
-//     // O = |parameters| ^ stepsPerParameterRange * simsPerParameterSet ...I think 🤔
-//     let raw = [];
-//     for (i of range(simsPerParameterSet)) {
-//         raw.push(simulate());
-//     };
-//     return {
-//         raw,
-//     };
-// };
+// Never got around to finishing this part, just running individual sims was enough.
+function monteCarlo (
+    simsPerParameterSet     = 1,
+    // stepsPerParameterRange  = 10,
+    numberOfGamesRange      = { min: 3,     max: 50,        resolution: 1, },
+    numberOfPlayersRange    = { min: 100,   max: 1_000,   resolution: 100, },
+    playerEngagementRange   = { min: .1,    max: 1,         resolution: .1, },
+    scoreRandomnessRange    = { min: 0,     max: 1,         resolution: .1, },
+) {
+    let raw = [];
+    
+    // console.log(stepsInParameterRange(numberOfGamesRange))
+    // console.log(stepsInParameterRange(numberOfPlayersRange))
+    // console.log(stepsInParameterRange(playerEngagementRange))
+    // console.log(stepsInParameterRange(scoreRandomnessRange))
+    for (const iG of stepsInParameterRange(numberOfGamesRange)) {
+        for (const iP of stepsInParameterRange(numberOfPlayersRange)) {
+            for (const i of range(simsPerParameterSet)) {
+                raw.push(simulate(iG, iP, scoreRandomnessRange, playerEngagementRange));
+            };
+        };
+    };
+    return {
+        raw,
+    };
+};
 
 function simulate (
     numberOfGames,
@@ -41,14 +50,17 @@ function simulate (
     randomnessRange,
     playerEngagementRange,
 ) {
-    console.log(`Simulating ${numberOfPlayers} players playing ${numberOfGames} games...`)
+    console.log(`Simulating ${numberOfPlayers} players playing ${numberOfGames} games with ${randomnessRange.min}-${randomnessRange.max} scoring randomness and ${playerEngagementRange.min}-${playerEngagementRange.max}...`)
     const players = range(numberOfPlayers).map(i => player(i, playerEngagementRange))
     const games = range(numberOfGames).map(i => game(i, randomnessRange));
 
     // Players randomly decide which games they will play
     // NOTE: this is probably slow AF
     for (const player of players) {
-        const gameCount = Math.ceil(games.length * player.participation);
+        // Players can't conceivably play more than a certain number of games due to time constraints
+        // Would be good to have a # of plays factor... participation is this to a degree,
+        // But if feels like it doesn't capture going deep vs going wide very well
+        const gameCount = Math.min(Math.ceil(games.length * player.participation), 15);
         player.games = [];
         for (let i = 0; player.games.length < gameCount; i++) {
             if (Math.random() >= .5) {
@@ -73,12 +85,12 @@ function simulate (
         };
     };
 
-    console.log(
-        'The players play the games',
-        // scores,
-    );
+    // console.log(
+    //     'The players play the games',
+    //     // scores,
+    // );
 
-    // Calculate leaderboards, percentiles and podiums for all games
+    // Calculate leaderboards, % performance and podiums for all games
     const tabulatedScores = games.reduce((agg, game) => {
         agg[game.id] = tabulateGameScores(game.id, scores)
         return agg;
@@ -88,7 +100,7 @@ function simulate (
     const normalizedScores = games.reduce((agg, game) => {
         agg[game.id] = players.reduce((agg, player) => {
             agg[player.id] = calculateGameScoreComponent(
-                tabulatedScores[game.id].percentiles[player.id],
+                tabulatedScores[game.id].normalizedPerformances[player.id],
                 tabulatedScores[game.id].leaderboard.findIndex(score => score.player === player.id) + 1,
             );
             return agg;
@@ -96,17 +108,17 @@ function simulate (
         return agg;
     }, {});
 
-    console.log(
-        'Give players normalized scores for all games',
-        // normalizedScores,
-    );
+    // console.log(
+    //     'Give players normalized scores for all games',
+    //     // normalizedScores,
+    // );
 
-    // Calculate leaderboard, percentiles and podium for overall metascores
+    // Calculate leaderboard, % performance and podium for overall metascores
     const metascores = tabulateMetascores(players, normalizedScores);
     
     console.log(
-        'Calculate overall scores',
-        metascores.podium,
+        'Calculate overall scores...',
+        // metascores.podium,
         `${metascores.elite.length} elite ranked players,`,
         `${metascores.strong.length} strong ranked players,`,
         `${metascores.wooden.length} wooden ranked players,`,
@@ -115,17 +127,17 @@ function simulate (
 
     const participation = tabulateParticipation(players, games, metascores);
 
-    console.log(
-        'Tabulate participation',
-        participation
-    )
+    // console.log(
+    //     'Tabulate participation',
+    //     participation
+    // )
 
     const performance = tabulatePerformance(players, games, metascores, tabulatedScores)
 
-    console.log(
-        'Tabulate performance',
-        performance
-    )
+    // console.log(
+    //     'Tabulate performance',
+    //     performance
+    // )
 
     return {
         // Input parameters
@@ -190,32 +202,52 @@ function tabulatePerformance (
             thirds: countPlayerPlacing(player, 2, 2),
         }
     };
-    function getAveragePercentile (player) {
+    function getAveragePerformance (player) {
         return Object.keys(tabulatedScores).reduce((agg, game) => {
-            agg += tabulatedScores[game].percentiles[player] || 0;
+            agg += tabulatedScores[game].normalizedPerformances[player] || 0;
             return agg;
         }, 0) / players.find(x => x.id === player).games.length;
     };
     const rank1 = {
         ...tabulatePodiums(metascores.podium.first.player),
-        averagePercentile: getAveragePercentile(metascores.podium.first.player),
+        averagePerformance: getAveragePerformance(metascores.podium.first.player),
     };
     const rank2 = {
         ...tabulatePodiums(metascores.podium.second.player),
-        averagePercentile: getAveragePercentile(metascores.podium.second.player),
+        averagePerformance: getAveragePerformance(metascores.podium.second.player),
     };
     const rank3 = {
         ...tabulatePodiums(metascores.podium.third.player),
-        averagePercentile: getAveragePercentile(metascores.podium.third.player),
+        averagePerformance: getAveragePerformance(metascores.podium.third.player),
+    };
+    const elite = {
+        averagePerformance: metascores.elite.reduce((agg, player, i) => {
+            return agg + getAveragePerformance(player.id);
+        }, 0) / metascores.elite.length,
+    };
+    const strong = {
+        averagePerformance: metascores.strong.reduce((agg, player, i) => {
+            return agg + getAveragePerformance(player.id);
+        }, 0) / metascores.strong.length,
+    };
+    const wooden = {
+        averagePerformance: metascores.wooden.reduce((agg, player, i) => {
+            return agg + getAveragePerformance(player.id);
+        }, 0) / metascores.wooden.length,
+    };
+    const unranked = {
+        averagePerformance: metascores.unranked.reduce((agg, player, i) => {
+            return agg + getAveragePerformance(player.id);
+        }, 0) / metascores.unranked.length,
     };
     return {
         rank1,
         rank2,
         rank3,
-        // elite,
-        // strong,
-        // wooden,
-        // podium,
+        elite,
+        strong,
+        wooden,
+        unranked,
     };
 };
 
@@ -235,6 +267,10 @@ function tabulateParticipation (
     const rank2 = rate(players.find(x => x.id === metascores.podium.second.player), games);
     const rank3 = rate(players.find(x => x.id === metascores.podium.third.player), games);
     const podium = (rank1 + rank2 + rank3) / 3;
+    const unranked = metascores.unranked.reduce((agg, player) => {
+        agg += rate(player, games);
+        return agg;
+    }, 0) / metascores.unranked.length;
     const wooden = metascores.wooden.reduce((agg, player) => {
         agg += rate(player, games);
         return agg;
@@ -256,6 +292,7 @@ function tabulateParticipation (
         strong,
         wooden,
         podium,
+        unranked,
     };
 }
 
@@ -265,8 +302,8 @@ function tabulateGameScores (
 ) {
     scores = scores.filter(score => score.game === game);
     const leaderboard = scores.sort((a, b) => b.score - a.score);
-    const percentiles = scores.reduce((agg, score, i) => {
-        agg[score.player] = 1 - (i / (leaderboard.length - 1));
+    const normalizedPerformances = scores.reduce((agg, score, i) => {
+        agg[score.player] = score.score / leaderboard[0].score;
         return agg;
     }, {});
     const podium = {
@@ -276,7 +313,7 @@ function tabulateGameScores (
     };
     return {
         leaderboard,
-        percentiles,
+        normalizedPerformances,
         podium,
     };
 };
@@ -302,12 +339,13 @@ function tabulateMetascores (
     .reduce((agg, player) => {
         agg.push({
             player: player.id,
-            score: scores[player.id]
+            score: scores[player.id],
+            skill: player.skill,
         });
         return agg;
     }, []);
-    const percentiles = leaderboard.reduce((agg, x, i) => {
-        agg[x.player] = 1 - i / (players.length - 1)
+    const normalizedPerformances = leaderboard.reduce((agg, x, i) => {
+        agg[x.player] = x.score / leaderboard[0].score
         return agg;
     }, {});
     const podium = {
@@ -320,20 +358,20 @@ function tabulateMetascores (
     });
     const wooden = players.filter(player => {
         return scores[player.id] >= config.tier1ScoreThreshold
-            && percentiles[player.id] <= config.tier2Percentile;
+            && normalizedPerformances[player.id] <= config.tier2Percentile;
     });
     const strong = players.filter(player => {
-        return percentiles[player.id] > config.tier2Percentile
-            && percentiles[player.id] <= config.tier3Percentile;
+        return normalizedPerformances[player.id] > config.tier2Percentile
+            && normalizedPerformances[player.id] <= config.tier3Percentile;
     });
     const elite = players.filter(player => {
-        return percentiles[player.id] > config.tier3Percentile
+        return normalizedPerformances[player.id] > config.tier3Percentile
             && leaderboard.findIndex(x => x.player === player.id) > 2;
     });
     return {
         scores,
         leaderboard,
-        percentiles,
+        normalizedPerformances,
         podium,
         unranked,
         wooden,
@@ -345,11 +383,10 @@ function tabulateMetascores (
 // Mock metascore functions
 
 function calculateGameScoreComponent (
-    percentile,
+    normalizedPerformance,
     rank,
     {
         max,
-        participationPortion,
         performancePortion,
         podiumPortion,
         secondPlaceDenominator,
@@ -357,17 +394,17 @@ function calculateGameScoreComponent (
     } = defaultScoringConfig,
 ) {
     if (
-        participationPortion
-        + performancePortion
+        performancePortion
         + podiumPortion
         !== 1
     ) throw new Error('Scoring portions must sum to 1.');
     if (
-        percentile === undefined ||
+        normalizedPerformance === undefined ||
         rank === undefined
     ) return 0;
-    let score = max * participationPortion;
-    score += max * performancePortion * percentile;
+    // First portion of score is awarded based on (player score ÷ top score)
+    let score = max * normalizedPerformance * performancePortion;
+    // Second portion of score is awarded based on podium placement
     if (rank === 3) score += max * podiumPortion / thirdPlaceDenominator;
     if (rank === 2) score += max * podiumPortion / secondPlaceDenominator;
     if (rank === 1) score += max * podiumPortion;
@@ -395,7 +432,12 @@ function stepsInParameterRange (
         interval = allowFloats ? interval : Math.floor(interval);
     }
     let steps = [];
-    return 
+    let agg = range.min;
+    while (agg <= range.max) {
+        steps.push(agg);
+        agg += interval;
+    }
+    return steps;
 };
 
 function range (n) {
